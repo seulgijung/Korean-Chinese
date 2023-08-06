bc2bc43c576349b06985d4627f3549af7bcb829e
seulgijung <seulgi.jung1109@gmail.com>
2023-08-05 21:23:06 -0400

    Update line-data.js

--- data/line-data.js
+++ data/line-data.js
@@ -9,27 +9,5 @@ const lineData = [{
     ]
 }]
 
-// Convert the year string to a number
-lineData[0].values.forEach(item => {
-    item.year = parseInt(item.year);
-});
-
 
 export default lineData;
-
-// const lineData = [{
-//     values: [
-//         {date: "2020/01/01", total: 230},
-//         {date: "2020/02/01", total: 290},
-//         {date: "2020/03/01", total: 230},
-//         {date: "2020/04/01", total: 270},
-//         {date: "2020/05/01", total: 244},
-//         {date: "2020/06/01", total: 270},
-//         {date: "2020/07/01", total: 320},
-//         {date: "2020/08/01", total: 320},
-//         {date: "2020/09/01", total: 250},
-//         {date: "2020/10/01", total: 272},
-//     ]
-// }]
-
-// export default lineData;
